03c931a10bdf780348d906e7006e564b6e5fff1c
steelbrain <de.steelbrain@gmail.com>
2016-02-07 09:34:43 +0500

    :new: Add basic specs for providers-list::trigger

--- spec/providers-list-spec.js
+++ spec/providers-list-spec.js
@@ -4,12 +4,19 @@ import {ProvidersList} from '../lib/providers-list'
 
 describe('ProvidersList', function() {
   let providersList
+  let editor
 
   beforeEach(function() {
     if (providersList) {
       providersList.dispose()
     }
     providersList = new ProvidersList()
+    atom.workspace.destroyActivePane()
+    waitsForPromise(function() {
+      return atom.workspace.open(__filename).then(function() {
+        editor = atom.workspace.getActiveTextEditor()
+      })
+    })
   })
 
   describe('addProvider', function() {
@@ -107,6 +114,76 @@ describe('ProvidersList', function() {
       expect(providersList.hasProvider(provider)).toBe(true)
       providersList.deleteProvider(provider)
       expect(providersList.hasProvider(provider)).toBe(false)
+    })
+  })
+  describe('trigger', function() {
+    it('works properly', function() {
+      const intention  = {
+        priority: 100,
+        icon: 'bucket',
+        class: 'custom-icon-class',
+        title: 'Choose color from colorpicker',
+        selected: function() {
+          console.log('You clicked the color picker option')
+        }
+      }
+      providersList.addProvider({
+        grammarScopes: ['*'],
+        getIntentions: function() {
+          return [intention]
+        }
+      })
+      waitsForPromise(function() {
+        return providersList.trigger(editor).then(function(results) {
+          expect(results).not.toBe(null)
+          expect(results instanceof Array).toBe(true)
+          expect(results[0]).toBe(intention)
+        })
+      })
+    })
+    it('ignores previous result from executed twice instantly', function() {
+      let count = 0
+      const intentionFirst  = {
+        priority: 100,
+        icon: 'bucket',
+        class: 'custom-icon-class',
+        title: 'Choose color from colorpicker',
+        selected: function() {
+          console.log('You clicked the color picker option')
+        }
+      }
+      const intentionSecond  = {
+        priority: 100,
+        icon: 'bucket',
+        class: 'custom-icon-class',
+        title: 'Choose color from colorpicker',
+        selected: function() {
+          console.log('You clicked the color picker option')
+        }
+      }
+      providersList.addProvider({
+        grammarScopes: ['*'],
+        getIntentions: function() {
+          if (++count === 1) {
+            return [intentionFirst]
+          } else return [intentionSecond]
+        }
+      })
+      const promiseFirst = providersList.trigger(editor)
+      const promiseSecond = providersList.trigger(editor)
+
+      waitsForPromise(function() {
+        return promiseFirst.then(function(results) {
+          expect(results).toBe(null)
+        })
+      })
+      waitsForPromise(function() {
+        return promiseSecond.then(function(results) {
+          expect(results).not.toBe(null)
+          expect(results instanceof Array).toBe(true)
+          expect(results[0]).toBe(intentionSecond)
+        })
+      })
     })
   })
 })
