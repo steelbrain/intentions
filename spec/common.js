5f71f5970ee07ef27f82b22ef40b62c4f57a1783
steelbrain <de.steelbrain@gmail.com>
2016-02-15 20:25:19 +0000

    :new: Add async spec helpers

--- spec/common.js
+++ spec/common.js
@@ -25,3 +25,25 @@ export function triggerKeyboardEvent(element, code, name = 'keydown') {
   })
   element.dispatchEvent(event)
 }
+
+export function waitsForAsync(asynCallback, result) {
+  waitsForPromise(function () {
+    return asynCallback().then(function (returnValue) {
+      if (typeof result !== 'undefined') {
+        expect(returnValue).toEqual(result)
+      }
+    })
+  })
+}
+
+export function waitsForAsyncRejection(asynCallback, errorMessage) {
+  waitsForPromise(function () {
+    return asynCallback().then(function () {
+      expect(false).toBe(true)
+    }, function (error) {
+      if (typeof errorMessage !== 'undefined') {
+        expect(error.message).toEqual(errorMessage)
+      }
+    })
+  })
+}
