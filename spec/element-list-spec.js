04c41809e30ff46fa56f8fe5a38dece35326db99
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-06-16 14:47:56 -0500

    test: rewrite the element-list-spec

--- spec/element-list-spec.js
+++ spec/element-list-spec.js
@@ -1,64 +1,65 @@
 /* @flow */
 import "module-alias/register"
 
-import ListElement from "../dist/elements/list"
+import { ListElement } from "../dist/elements/list"
 import { createSuggestion } from "./helpers"
+import { createMutable } from "solid-js"
+import { render } from "solid-js/web"
 
 describe("Intentions list element", function () {
   it("has a complete working lifecycle", function () {
-    const element = new ListElement()
+    const rootElement = document.createElement("div")
+
     const suggestions = [
       createSuggestion("Suggestion 1", jasmine.createSpy("suggestion.selected.0"), "someClass", "someIcon"),
       createSuggestion("Suggestion 2", jasmine.createSpy("suggestion.selected.1")),
       createSuggestion("Suggestion 3", jasmine.createSpy("suggestion.selected.2"), "anotherClass"),
     ]
+    const selectCallback = jasmine.createSpy("suggestion.selected")
+    const props = createMutable({ suggestions, selectCallback, movement: "move-to-top" })
 
-    const selected = jasmine.createSpy("suggestion.selected")
-    const rendered = element.render(suggestions, selected)
+    render(() => ListElement(props), rootElement)
 
-    expect(rendered.refs.list.children.length).toBe(3)
-    expect(rendered.refs.list.children[0].textContent).toBe("Suggestion 1")
-    expect(rendered.refs.list.children[1].textContent).toBe("Suggestion 2")
-    expect(rendered.refs.list.children[2].textContent).toBe("Suggestion 3")
-    expect(rendered.refs.list.children[0].children[0].className).toBe("someClass icon icon-someIcon")
-    expect(rendered.refs.list.children[2].children[0].className).toBe("anotherClass")
-    expect(element.suggestionsIndex).toBe(-1)
+    const intentionList = rootElement.querySelector("#intentions-list")
+    const olElement = intentionList.firstElementChild
 
-    element.move("down")
+    expect(olElement.children.length).toBe(3)
+    expect(olElement.children[0].textContent).toBe("Suggestion 1")
+    expect(olElement.children[1].textContent).toBe("Suggestion 2")
+    expect(olElement.children[2].textContent).toBe("Suggestion 3")
+    expect(olElement.children[0].children[0].className).toBe("someClass icon icon-someIcon")
+    expect(olElement.children[2].children[0].className).toBe("anotherClass")
 
-    expect(element.suggestionsIndex).toBe(0)
-    expect(element.suggestions[element.suggestionsIndex].title).toBe(rendered.refs.list.children[0].textContent)
+    props.movement = "down"
 
-    element.move("down")
+    expect(suggestions[0].title).toBe(olElement.children[0].textContent)
 
-    expect(element.suggestionsIndex).toBe(1)
-    expect(element.suggestions[element.suggestionsIndex].title).toBe(rendered.refs.list.children[1].textContent)
+    props.movement = "down"
 
-    element.move("down")
+    expect(suggestionsIndex).toBe(1)
+    expect(suggestions[1].title).toBe(olElement.children[1].textContent)
 
-    expect(element.suggestionsIndex).toBe(2)
-    expect(element.suggestions[element.suggestionsIndex].title).toBe(rendered.refs.list.children[2].textContent)
+    props.movement = "down"
 
-    element.move("up")
+    expect(suggestions[2].title).toBe(olElement.children[2].textContent)
 
-    expect(element.suggestionsIndex).toBe(1)
-    expect(element.suggestions[element.suggestionsIndex].title).toBe(rendered.refs.list.children[1].textContent)
+    props.movement = "up"
 
-    element.move("up")
+    expect(suggestions[1].title).toBe(olElement.children[1].textContent)
 
-    expect(element.suggestionsIndex).toBe(0)
-    expect(element.suggestions[element.suggestionsIndex].title).toBe(rendered.refs.list.children[0].textContent)
+    props.movement = "up"
 
-    element.move("up")
+    expect(suggestions[0].title).toBe(olElement.children[0].textContent)
 
-    expect(element.suggestionsIndex).toBe(2)
-    expect(element.suggestions[element.suggestionsIndex].title).toBe(rendered.refs.list.children[2].textContent)
+    props.movement = "up"
 
-    rendered.refs.list.children[1].children[0].dispatchEvent(
+    expect(suggestions[2].title).toBe(olElement.children[2].textContent)
+
+    olElement.children[1].children[0].dispatchEvent(
       new MouseEvent("click", {
         bubbles: true,
       })
     )
-    expect(selected).toHaveBeenCalledWith(suggestions[1])
+    expect(selectCallback).toHaveBeenCalledWith(suggestions[1])
   })
 })
