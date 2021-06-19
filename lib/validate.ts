6f99b733cb699be3a84584b72d57fecc9c2912b3
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-06-18 22:09:11 -0500

    fix: remove unnecessary null-check in validate functions

--- lib/validate.ts
+++ lib/validate.ts
@@ -3,7 +3,7 @@ import type { ListProvider, ListItem, HighlightProvider, HighlightItem } from ".
 export function provider(entry: ListProvider | HighlightProvider) {
   let message
 
-  if (!entry || typeof entry !== "object") {
+  if (typeof entry !== "object") {
     message = "Invalid provider provided"
   } else if (!Array.isArray(entry.grammarScopes)) {
     message = "Invalid or no grammarScopes found on provider"
@@ -47,7 +47,7 @@ export function suggestionsShow(suggestions: Array<HighlightItem>): Array<Highli
       const suggestion = suggestions[i]
       let message
 
-      if (typeof suggestion.range !== "object" || !suggestion.range) {
+      if (typeof suggestion.range !== "object") {
         message = "Invalid or no range found on intention"
       } else if (suggestion.class !== undefined && typeof suggestion.class !== "string") {
         message = "Invalid class found on intention"
